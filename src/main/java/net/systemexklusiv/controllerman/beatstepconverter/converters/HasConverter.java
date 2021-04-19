873d59571632d9b27b42c058316ce1457d142da9
David Rival <sytemexklusiv@gmail.com>
2021-04-19 09:40:14 +0200

    add comment to class

--- src/main/java/net/systemexklusiv/controllerman/beatstepconverter/converters/HasConverter.java
+++ src/main/java/net/systemexklusiv/controllerman/beatstepconverter/converters/HasConverter.java
@@ -5,10 +5,10 @@ import net.systemexklusiv.controllerman.beatstepconverter.ControllerEntry.Contro
 
 public interface HasConverter {
     /**
-     * @param currentControllerType pass in the actual to check if to be conerted or not
-     * @param beatstepFeatureNum pass in the feature num (1-6) in oder to check if it is the right uple for the conversion
-     * @param value pass in the actual value of the tuple, pass it throughnif no conversion is to be done
-     * @return
+     * @param currentControllerType pass in the actual to check if to be conevrted or not
+     * @param beatstepFeatureNum pass in the feature num (1-6) in oder to check if it is the right tuple for the conversion
+     * @param value pass in the actual value of the tuple, pass it through if no conversion is to be done
+     * @return the converted value, eg the new midichannel or CC/Note number
      */
     String convert(ControllerType currentControllerType, int beatstepFeatureNum, String value);
 
